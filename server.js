c674d07d70527322825dfc5899fedc7595eb5dc2
dwivedidotred <shivamdwivedi618@gmail.com>
2019-03-03 00:16:25 +0530

    trex upadated

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express'),
-path = request('path');
+    path = require('path');
 const app = express();
-app.use(express.static('.dist/dummy'));
+app.use(express.static('./dist/trex'));
 
 app.get('/*', (req,res) =>{
-    res.sendFile(path.join(__dirname,'/dist/dummy/index.html'));
+    res.sendFile(path.join(__dirname,'/dist/trex/index.html'));
 
 });
 app.listen(process.env.PORT || 4200,()=>{
